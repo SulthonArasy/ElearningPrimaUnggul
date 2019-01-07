2bf756f71ac3d4693f988886336d35145be915c2
jokopriyono <joe.mandrake007@gmail.com>
2019-01-08 06:18:03 +0700

    - Memperbaiki proses upload file materi

--- app/src/main/java/com/sulthon/elearningprimaunggul/ui/uploadmateri/UploadMateriActivity.java
+++ app/src/main/java/com/sulthon/elearningprimaunggul/ui/uploadmateri/UploadMateriActivity.java
@@ -5,86 +5,51 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
+import com.sulthon.elearningprimaunggul.CommonHelper;
 import com.sulthon.elearningprimaunggul.R;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
 
-/**
- * Created by Kuncoro on 25/09/2017.
- * <a class="vglnk" href="http://www.dedykuncoro.com" rel="nofollow"><span>www</span><span>.</span><span>dedykuncoro</span><span>.</span><span>com</span></a>
- */
-
-public class UploadMateriActivity extends AppCompatActivity {
-
-    public final int REQUEST_CAMERA = 0;
-    public final int SELECT_FILE = 1;
-    Intent intent;
-    Uri fileUri;
-    Button btn_choose_image;
-    ImageView imageView;
-    Bitmap bitmap, decoded;
-    int bitmap_size = 40; // image quality 1 - 100;
-    int max_resolution_image = 800;
-
-    private static File getOutputMediaFile() {
-
-        // External sdcard location
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "DeKa");
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists()) {
-            if (!mediaStorageDir.mkdirs()) {
-                Log.e("Monitoring", "Oops! Failed create Monitoring directory");
-                return null;
-            }
-        }
-
-        // Create a media file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
-        File mediaFile;
-        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_DeKa_" + timeStamp + ".jpg");
-
-        return mediaFile;
-    }
+public class UploadMateriActivity extends AppCompatActivity implements View.OnClickListener {
+    public final int REQUEST_FILE = 200;
+    private TextView txtFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_materi);
 
-        btn_choose_image = (Button) findViewById(R.id.btn_choose_image);
-        imageView = (ImageView) findViewById(R.id.image_view);
+        Button btnChooseFile = findViewById(R.id.btn_choose_file);
+        Button btnUpload = findViewById(R.id.btn_input_file);
+        EditText edtPelajaran = findViewById(R.id.edt_pelajaran);
+        EditText edtMateri = findViewById(R.id.edt_materi);
+        EditText edtDeskripsi = findViewById(R.id.edt_deskripsi);
+        txtFile = findViewById(R.id.txt_file);
 
-        btn_choose_image.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnChooseFile.setOnClickListener(this);
+        btnUpload.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_choose_file:
                 new TedPermission(UploadMateriActivity.this)
                         .setPermissionListener(new PermissionListener() {
                             @Override
                             public void onPermissionGranted() {
-                                selectImage();
+                                selectFile();
                             }
 
                             @Override
@@ -97,26 +62,30 @@ public class UploadMateriActivity extends AppCompatActivity {
                                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
                                 Manifest.permission.READ_EXTERNAL_STORAGE)
                         .check();
-            }
-        });
-
+                break;
+            case R.id.btn_input_file:
+                break;
+        }
     }
 
-    private void selectImage() {
-        imageView.setImageResource(0);
-        final CharSequence[] items = {"Choose from Library", "Cancel"};
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("*/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Choose File to Upload.."), REQUEST_FILE);
+    }
+
+    private void selectFile() {
+        final CharSequence[] items = {"Pilih file", "Batal"};
 
         AlertDialog.Builder builder = new AlertDialog.Builder(UploadMateriActivity.this);
-        builder.setTitle("Add Photo!");
+        builder.setTitle("Pilih file..");
         builder.setIcon(R.mipmap.ic_launcher);
         builder.setItems(items, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int item) {
-                if (items[item].equals("Choose from Library")) {
-                    intent = new Intent();
-                    intent.setType("image/*");
-                    intent.setAction(Intent.ACTION_GET_CONTENT);
-                    startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_FILE);
+                if (items[item].equals("Pilih file")) {
+                    showFileChooser();
                 } else {
                     dialog.dismiss();
                 }
@@ -126,45 +95,19 @@ public class UploadMateriActivity extends AppCompatActivity {
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.e("onActivityResult", "requestCode " + requestCode + ", resultCode " + resultCode);
-
         if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == SELECT_FILE && data != null && data.getData() != null) {
-                try {
-                    bitmap = MediaStore.Images.Media.getBitmap(UploadMateriActivity.this.getContentResolver(), data.getData());
-                    setToImageView(getResizedBitmap(bitmap, max_resolution_image));
-                } catch (IOException e) {
-                    e.printStackTrace();
+            if (requestCode == REQUEST_FILE) {
+                if (data == null) {
+                    return;
+                }
+                Uri selectedFileUri = data.getData();
+                String selectedFilePath = CommonHelper.getPath(this, selectedFileUri);
+                if (selectedFilePath != null && !selectedFilePath.equals("")) {
+                    txtFile.setText(selectedFilePath);
+                } else {
+                    Toast.makeText(this, "Cannot upload file to server", Toast.LENGTH_SHORT).show();
                 }
             }
         }
     }
-
-    // Untuk menampilkan bitmap pada ImageView
-    private void setToImageView(Bitmap bmp) {
-        //compress image
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.JPEG, bitmap_size, bytes);
-        decoded = BitmapFactory.decodeStream(new ByteArrayInputStream(bytes.toByteArray()));
-
-        //menampilkan gambar yang dipilih dari camera/gallery ke ImageView
-        imageView.setImageBitmap(decoded);
-    }
-
-    // Untuk resize bitmap
-    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
-        int width = image.getWidth();
-        int height = image.getHeight();
-
-        float bitmapRatio = (float) width / (float) height;
-        if (bitmapRatio > 1) {
-            width = maxSize;
-            height = (int) (width / bitmapRatio);
-        } else {
-            height = maxSize;
-            width = (int) (height * bitmapRatio);
-        }
-        return Bitmap.createScaledBitmap(image, width, height, true);
-    }
-
 }
